a790916e7ac09f4e26acae9807583cfb57de9bb0
Robin <rohefner@stud.hs-heilbronn.de>
2024-06-05 20:40:31 +0200

    Added Cats And Dogs

---
+++ 3_ConvolutionalNeuralNetwork/CatsAndDogs/usingCNNCatsAndDogs.py
@@ -0,0 +1,8 @@
+import os
+import zipfile
+import random
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile
+#https://colab.research.google.com/github/lmoroney/mlday-tokyo/blob/master/Lab6-Cats-v-Dogs.ipynb#scrollTo=zvSODo0f9LaU
